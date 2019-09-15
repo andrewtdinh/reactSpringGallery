da6f82393205e2eced31a38b1341ef132545f4f1
Andrew Dinh <adinh@sightmachine.com>
2019-09-14 22:47:23 -0700

    Set up animated stars

--- tutorial/src/StarRating/index.js
+++ tutorial/src/StarRating/index.js
@@ -5,6 +5,15 @@ import Star from "../Star";
 export const StarRating = ({ rating, setRating }) => {
   // Make <Star /> component aware of animations
   const AnimatedStar = animated(Star);
+  const animatedStars = useTrail(5, {
+    config: {
+      friction: 22,
+      tension: 500
+    },
+    from: { opacity: 0, transform: "scale(0.8)" },
+    opacity: 1,
+    transform: "scale(1)"
+  });
 
   // Set stars to animate one after another
 
